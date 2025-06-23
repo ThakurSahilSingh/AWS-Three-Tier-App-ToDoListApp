4c4586b1c2ebd7757b957323f64e87d5578ba22a
ThakurSahilSingh <t.sahilsingh2429@gmail.com>
2025-06-23 22:43:15 +0000

    Updated

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const apiUrl = "/api/tasks";
+const apiUrl = "http://acc307c6d9e2347f18076949a8546f86-1889230027.us-east-1.elb.amazonaws.com/api/tasks";
+
 
 export function getTasks() {
     return axios.get(apiUrl);
